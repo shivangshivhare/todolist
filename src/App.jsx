aead94839108c3d034cad7555af7bf4216e5475f
John Doe <shivang raj shivhare>
2024-05-22 17:56:32 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useState } from "react";
+import Header from "./components/Header";
+import Create from "./components/Create";
+import Show from "./components/Show";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const App = () => {
+    const [tasks, settasks] = useState(
+        JSON.parse(localStorage.getItem("tasks")) || []
+    );
 
-export default App
+    return (
+        <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
+            {/*  */}
+            <Header />
+            {/*  */}
+            <Create tasks={tasks} settasks={settasks} />
+            {/*  */}
+            <Show tasks={tasks} settasks={settasks} />
+        </div>
+    );
+};
+
+export default App;
